e3a6163d61f4373b9f78610a53c7979865baba0b
Avnish Patel <48069185+AVNISHPATEL102@users.noreply.github.com>
2019-11-06 11:09:34 -0500

    Add files via upload

---
+++ PA2/Task_4/Semaphore.java
@@ -0,0 +1,42 @@
+package Task_4;
+
+class Semaphore{
+	private int value;
+	
+	public Semaphore() {
+		this(0);
+	}
+	public Semaphore(int value) {
+		if (this.value < 0) {
+			this.value = 0;
+		} else {
+			this.value = value;
+		}
+	}
+
+	public synchronized void Wait() {
+		if (this.value < 0) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				System.out.println("Semaphore::Wait() - caught InterruptedException: " + e.getMessage());
+				e.printStackTrace();
+			}
+		}
+		this.value--;
+
+	}
+
+	public synchronized void Signal() {
+		++this.value;
+		notify();
+	}
+
+	public synchronized void P() {
+		this.Wait();
+	}
+
+	public synchronized void V() {
+		this.Signal();
+	}
+}
