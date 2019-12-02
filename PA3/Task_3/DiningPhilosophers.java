9980795ddceda58488533fc12936c1135b3faf9e
Avnish Patel <48069185+AVNISHPATEL102@users.noreply.github.com>
2019-12-02 09:41:45 -0500

    Add files via upload

---
+++ PA3/Task_3/DiningPhilosophers.java
@@ -0,0 +1,116 @@
+package Task_3;
+import java.util.Scanner;
+
+public class DiningPhilosophers
+{
+	/*
+	 * ------------
+	 * Data members
+	 * ------------
+	 */
+
+	/**
+	 * This default may be overridden from the command line
+	 */
+	public static final int DEFAULT_NUMBER_OF_PHILOSOPHERS = 4;
+
+	/**
+	 * Dining "iterations" per philosopher thread
+	 * while they are socializing there
+	 */
+	//changed it to one to make life easier and to make the diner finish faster (some of them just came for the free food)
+	public static final int DINING_STEPS = 1;
+
+	/**
+	 * Our shared monitor for the philosphers to consult
+	 */
+	public static Monitor soMonitor = null;
+
+	/*
+	 * -------
+	 * Methods
+	 * -------
+	 */
+
+	/**
+	 * Main system starts up right here
+	 */
+	public static void main(String[] argv)
+	{
+		try
+		{
+			/*
+			 * TODO:
+			 * Should be settable from the command line
+			 * or the default if no arguments supplied.
+			 */
+			int Number = 0;
+			Scanner s = new Scanner(System.in);
+			System.out.print("Enter the number of Philosophers that you wish to gather around for a sad diner:");
+			String Input =s.nextLine();
+			if(Input=="" ) {
+				Number=DEFAULT_NUMBER_OF_PHILOSOPHERS;
+			}
+			else {
+				try {
+					Number = Integer.parseInt(Input);
+					if(Number<0) {
+						System.out.println("you entered a negative number, the default has been set (4 philosophers)");
+						Number=DEFAULT_NUMBER_OF_PHILOSOPHERS;
+					}
+				}
+				catch(NumberFormatException e){
+					System.out.println("You did not enter a number, dumb dumb");
+				}
+			}
+			int iPhilosophers =Number;
+
+			// Make the monitor aware of how many philosophers there are
+			soMonitor = new Monitor(iPhilosophers);
+
+			// Space for all the philosophers
+			Philosopher aoPhilosophers[] = new Philosopher[iPhilosophers];
+
+			// Let 'em sit down
+			for(int j = 0; j < iPhilosophers; j++)
+			{
+				aoPhilosophers[j] = new Philosopher();
+				aoPhilosophers[j].start();
+			}
+
+			System.out.println
+			(
+				iPhilosophers +
+				" philosopher(s) came in for a dinner."
+			);
+
+			// Main waits for all its children to die...
+			// I mean, philosophers to finish their dinner.
+			for(int j = 0; j < iPhilosophers; j++)
+				aoPhilosophers[j].join();
+
+			System.out.println("All philosophers have left. System terminates normally.");
+		}
+		catch(InterruptedException e)
+		{
+			System.err.println("main():");
+			reportException(e);
+			System.exit(1);
+		}
+	} // main()
+
+	/**
+	 * Outputs exception information to STDERR
+	 * @param poException Exception object to dump to STDERR
+	 */
+	public static void reportException(Exception poException)
+	{
+		System.err.println("Caught exception : " + poException.getClass().getName());
+		System.err.println("Message          : " + poException.getMessage());
+		System.err.println("Stack Trace      : ");
+		poException.printStackTrace(System.err);
+	}
+}
+
+// EOF
+
